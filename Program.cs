0241918bce1731776b82ee7bb2eeccac798075c4
Uchimate <55698969+muriloferreirap@users.noreply.github.com>
2023-03-14 16:04:27 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,19 +8,17 @@ public class Program
     {
         Process[] proc = Process.GetProcesses();
 
-        string path = @"C:/PATHARQUIVO.txt";
+        string path = @"C:\Users\Murilo\Documents\projetos\dotnet_app\FirstApp\process.txt";
 
         using (StreamWriter writer = File.CreateText(path))
         {
             foreach (Process p in proc)
             {
-                writer.WriteLine(p.ProcessName);
-writer.WriteLine(p.ProcessId);
-
+                writer.WriteLine($"Name: {p.ProcessName} Id: {p.Id}");
             }
         }
 
-        Console.WriteLine("Nomes salvos com sucesso, verifique o caminho e o arquivo txt criado.")
+        Console.WriteLine("Nomes salvos com sucesso, verifique o caminho e o arquivo txt criado.");
         Console.ReadLine();
     }
 }
